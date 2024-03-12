80b0bb041aa70acd360e8f27ea49d0c651c5d290
Gabriel Lozano <golozanop@unal.edu.co>
2024-03-12 22:21:24 +0100

    will start to mess around

--- get_data.sh
+++ get_data.sh
@@ -5,16 +5,32 @@ set -eu
 
 # Do not throw error if folder already exists
 mkdir -p raw_data
+mkdir -p parsed_csv
 
 # Download the data
 cd raw_data
+# TODO uncomment
 # wget https://dblp.org/xml/dblp.dtd
 # wget https://dblp.org/xml/dblp.xml.gz -P raw_data
 
 # unpack the data
-gunzip -d dblp.xml.gz
+# TODO uncomment
+# gunzip -d dblp.xml.gz
 
 cd ..
 
 chmod +x src/xml2csv.py
-./src/xml2csv.py --annotate --neo4j raw_data/dblp.xml raw_data/dblp.dtd output.csv --relations author:authored_by journal:published_in publisher:published_by school:submitted_at editor:edited_by cite:has_citation series:is_part_of
+./src/xml2csv.py\
+    --annotate\
+    --neo4j \
+    raw_data/dblp.xml\
+    raw_data/dblp.dtd\
+    parsed_csv/output.csv \
+    --relations \
+    author:authored_by\
+    journal:published_in \
+    publisher:published_by \
+    school:submitted_at \
+    editor:edited_by \
+    cite:has_citation \
+    series:is_part_of
