e4521b37cb99ef5168e8fc7049c704f56b041805
Gabriel Lozano <37823629+Matematikoi@users.noreply.github.com>
2024-04-10 18:28:40 +0200

    Improves review process and adds updating the graph (#12)
    * Adds python script to evolve the graph
    
    * deletes useless library
    
    * adds new review process
    
    * Adds new script for review
    
    ---------
    
    Co-authored-by: Gabriel Lozano <golozanop@unal.edu.co>

--- src/reviewed_by.py
+++ src/reviewed_by.py
@@ -1,47 +1,48 @@
+import file_management as fm
+import pandas as pd
+import numpy as np
+from itertools import islice
+import csv
+
+
 import pandas as pd
 import numpy as np
 from itertools import islice
 import csv
 import re
-import file_management as fm
 
 def create_reviewed_by_relation():
     df = pd.read_csv("parsed_csv/output_author_authored_by.csv", delimiter=";",header=None, skiprows=1, names=[':START_ID', ':END_ID'])
+    author_map = {}
+    publication_map = {}
+    raw_values = df.values
+    for i in range(len(raw_values)):
+        if raw_values[i,0] not in author_map:
+            author_map[raw_values[i,0]] = [raw_values[i,1]]
+        else:
+            author_map[raw_values[i,0]].append(raw_values[i,1])
 
-    # Contar la frecuencia de los autores
-    author_counts = df[':END_ID'].value_counts()
-
-    # Obtener autores con al menos 5 artículos
-    author_more_5 = author_counts[author_counts >= 50].index.tolist()
-
-    # Tomar una muestra aleatoria de los artículos
-    sample_size = 10000
-    sample_df = df.sample(n=sample_size)
-
-    # Dividir el procesamiento en lotes más pequeños
-    batch_size = 1000
-    num_batches = int(sample_size / batch_size)
-
-    # Lista para almacenar las relaciones
-    relation = []
-
-    for i in range(num_batches):
-        batch_df = sample_df[i * batch_size : (i+1) * batch_size]
-        for article_id, group in batch_df.groupby(':START_ID'):
-            authors = group[':END_ID'].unique()
-            reviewer_selected = []
-            while len(reviewer_selected) < 3:
-                candidate = np.random.choice(author_more_5)
-                if candidate not in authors:
-                    reviewer_selected.append(candidate)
-
-            for reviewer in reviewer_selected:
-                relation.append({':START_ID': article_id, ':END_ID': reviewer})
-
-# Convertir la lista de relaciones en un DataFrame
-    relation_df = pd.DataFrame(relation)
-    
+        if raw_values[i,1] not in publication_map:
+            publication_map[raw_values[i,1]] = [raw_values[i,0]]
+        else:
+            publication_map[raw_values[i,1]].append(raw_values[i,0])
+    authors = list(author_map.keys())
+    publications = list(publication_map.keys())
+    reputable_authors = [i for i in authors if len(author_map[i])>35]
+    pub_aux, reviewed_aux = [],[]
+    for p in publications :
+        while True:
+            reviewers = np.random.choice(reputable_authors, size=3, replace = False)
+            for r in reviewers:
+                if r in publication_map:
+                    continue
+            break
+        for r in reviewers:
+            pub_aux.append(p)
+            reviewed_aux.append(r)
+    relation_df = pd.DataFrame({':START_ID':pub_aux,':END_ID':reviewed_aux})
     relation_df.to_csv("parsed_csv/output_reviewed_by.csv", index=False, header=True, sep = ';')  # Set index=False to exclude the index column
+
 
 
 def main():
---
+++ src/update_reviewed_by.py
@@ -0,0 +1,48 @@
+import run_queries as rq
+import asyncio
+
+def background(f):
+    def wrapped(*args, **kwargs):
+        return asyncio.get_event_loop().run_in_executor(None, f, *args, **kwargs)
+
+    return wrapped
+page_size = 10
+
+def get_review_info(page_size, offset):
+    reviews_query = f'''
+        MATCH p=(s)-[r:reviewed_by]->(e) RETURN id(s), id(e) skip {offset} LIMIT {page_size}
+    '''
+    reviews = rq.make_request(reviews_query,'gathering data')
+    return [ (i['row'][0], i['row'][1]) for i in reviews['results'][0]['data']]
+
+def get_reviews_formatted(reviews):
+    review_text = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'''
+    return '[{}]'.format(','.join([f'{{start: {i[0]},end: {i[1]},review: "{review_text}"}}' for i in reviews]))
+
+def update_data(review_data, total):
+    query = f'''
+        UNWIND {review_data} AS d
+        MATCH (s)-[r:reviewed_by]->(e) WHERE id(s) = d.start and id(e) = d.end
+        SET r.review = d.review
+        RETURN r.review
+    '''
+    result = rq.make_request(query, total)
+@background
+def update_offset(offset):
+    reviews = get_review_info(page_size, offset)
+    review_data = get_reviews_formatted(reviews)
+    update_data(review_data, offset + page_size )
+
+
+def update_review_by():
+    cnt = 0
+    offset = 0
+    while offset<1100000:
+        cnt +=1
+        offset += page_size
+        update_offset(offset)
+def main():
+    update_review_by()
+
+if __name__ == '__main__':
+    main()
