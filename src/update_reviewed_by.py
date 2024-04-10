596c1fc94a0f1dcfcb8416d4dd98887263ba6ad7
Gabriel Lozano <golozanop@unal.edu.co>
2024-04-10 18:46:55 +0200

    [HOTFIX] Adds decision to review

--- src/update_reviewed_by.py
+++ src/update_reviewed_by.py
@@ -1,4 +1,5 @@
 import run_queries as rq
+import numpy as np
 import asyncio
 
 def background(f):
@@ -17,13 +18,14 @@ def get_review_info(page_size, offset):
 
 def get_reviews_formatted(reviews):
     review_text = '''Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'''
-    return '[{}]'.format(','.join([f'{{start: {i[0]},end: {i[1]},review: "{review_text}"}}' for i in reviews]))
+    decision = "Approved" if np.random.rand() < 0.98 else "Denied"
+    return '[{}]'.format(','.join([f'{{start: {i[0]},end: {i[1]},review: "{review_text}", decision: "{decision}"}}' for i in reviews]))
 
 def update_data(review_data, total):
     query = f'''
         UNWIND {review_data} AS d
         MATCH (s)-[r:reviewed_by]->(e) WHERE id(s) = d.start and id(e) = d.end
-        SET r.review = d.review
+        SET r += {{review:d.review, decision:d.decision}}
         RETURN r.review
     '''
     result = rq.make_request(query, total)
